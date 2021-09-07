349f1770dc18364543e4aeceb65fa5450ec00336
Kasper <87650870+kasasper@users.noreply.github.com>
2021-09-07 15:00:28 +0200

    first step

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  render() {
+    return (
+      <div class="container">
+        <div class="header">
+        <h2>Elements</h2>
+        <form id="search-element">
+            <input type="text" id="search-element-input" placeholder="Search element..." onkeypress="return event.keyCode != 13;"></input>
+        </form>
+        <button class="button1"id="sampleDataButton">Generate sample data 1</button>
+        <button class="button1"id="sampleDataButton2">Generate sample data 2</button>
+        <button class="button1"id="clearLocalStorage">Remove all</button>
+        </div>
+        <div class="elements-row">
+            <span id="base">
+            </span>
+            <span class="btn-element new-element">
+                <p class="btn-element"><p class="add-new"><br></br>+ <br></br> ADD NEW ELEMENT</p></p>
+            </span>
+        </div>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
